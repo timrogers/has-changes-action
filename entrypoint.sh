e028ffbb531aba3b062b0466343460576117823e
Tim Rogers <me@timrogers.co.uk>
2022-05-17 18:25:50 +0100

    Allow customising the working directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,10 @@ set -e
 
 STATUS_ARGS=$1
 PATHSPEC=$2
+WORKING_DIRECTORY=$3
 
 function check() {
-  if [[ -z "$(git status --porcelain $STATUS_ARGS $PATHSPEC)" ]];
+  if [[ -z "$(cd $WORKING_DIRECTORY && git status --porcelain $STATUS_ARGS $PATHSPEC)" ]];
   then
     echo "0"
   else
